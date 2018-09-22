670121f0f9e56b1eb5f858b154b81baba2a32ccd
Nikolaos I. Tzanetis <tzanetis@outlook.com>
2018-09-22 05:22:16 +0300

    Changes in Associations 2, Set Optional Associations, Validations 2

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ApplicationRecord
-  belongs_to :continent
+  belongs_to :continent, optional: true # auto assign is nil, thus belongs to must be set to optional
   belongs_to :game
   belongs_to :ceo, class_name: 'User'
 
@@ -8,4 +8,15 @@ class Company < ApplicationRecord
   has_many :buyers, class_name: 'CompRnd', foreign_key: 'buyer_id'
   has_many :comp_qs
   has_many :comp_goals
+
+  validates :continent, presence: false, allow_nil: true
+  validates :ceo, uniqueness: {scope: :game}
+=begin
+  before_create :check_continent
+
+  private
+  def check_continent
+    self.continent_id = nil
+  end
+=end
 end
