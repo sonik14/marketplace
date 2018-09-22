0b0e0c5390bab1087fee2a7b48336949d56364f6
Nikolaos I. Tzanetis <tzanetis@outlook.com>
2018-09-22 05:27:32 +0300

    Function Associations, Validations (3) and Params Check with Methods.

--- app/models/function_q.rb
+++ app/models/function_q.rb
@@ -1,4 +1,13 @@
 class FunctionQ < ApplicationRecord
-  belongs_to :function
+  belongs_to :function, optional: true
+  belongs_to :function_usage
   belongs_to :quarter
+
+  validates :parA, numericality: true, format: { with: /\A\d+(?:\.\d{1,2})?\z/ }, allow_nil: true
+  validates :parB, numericality: true, format: { with: /\A\d+(?:\.\d{1,2})?\z/ }, allow_nil: true
+  validates :parC, numericality: true, format: { with: /\A\d+(?:\.\d{1,2})?\z/ }, allow_nil: true
+  validates :parD, numericality: true, format: { with: /\A\d+(?:\.\d{1,2})?\z/ }, allow_nil: true
+
+  include ApplicationHelper
+  before_update :check_function_params
 end
