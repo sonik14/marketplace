dfabc0b3c6be97c44b99c8d57c839416959a2819
Nikolaos I. Tzanetis <tzanetis@outlook.com>
2018-09-22 06:01:21 +0300

    Create Neccessary Models for New Game and Quarters. Simulation Proccess. Calculate and Extract Simulation Results.

--- app/admin/games.rb
+++ app/admin/games.rb
@@ -1,15 +1,183 @@
 ActiveAdmin.register Game do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+	permit_params :version_id, :first_month_id, :name, :duration
+	before_filter :ensure_admin!
 
+	# Disable edit and destroy actions
+	actions :index, :show, :new, :create
+
+	action_item :delete, only: :show do
+		q = Quarter.where(game_id: game.id).order(q_no: :desc).take
+		if q.nil?
+  			link_to "Delete Game", delete_admin_game_path(game), method: :put
+		end
+	end
+
+  	member_action :delete, method: :put do
+	  	game = Game.find(params[:id])
+	  	game.destroy
+	  	redirect_to admin_games_path
+	end
+
+	action_item :proceed, only: :show do
+	q = Quarter.where(game_id: game.id).order(q_no: :desc).take
+	if q.nil?
+  		link_to "Start Game", proceed_admin_game_path(game), method: :put, 
+		  		data: {confirm: "Please be sure that you have filled all the required parameters:\n-players of the game\nDo you want to start the game now?" } #, target: "_blank" if you want to open new window
+		elsif q.q_no < game.duration
+	  		link_to "Go to Next Quarter", proceed_admin_game_path(game), method: :put, 
+		    		data: {confirm: "Please be sure that you have filled all the required parameters:\n-function parameters for the game/quarter\n-function parameters depending on customer. \nDo you want to proceed to the following quarter?" }
+		elsif q.q_no == game.duration && !game.finished
+	  		link_to "Finish Game", proceed_admin_game_path(game), method: :put, 
+		    		data: {confirm: "Please be sure that you have filled all the required parameters:\n-function parameters for the game/quarter\n-function parameters depending on customer. \nDo you want to proceed to the final results?" }
+		end
+	end
+
+  	member_action :proceed, method: :put do
+	  	game = Game.find(params[:id])
+		q = Quarter.where(game_id: game.id).order(q_no: :desc).take
+		if q.nil? 
+			q_no = 1
+			Customer.all.each do |customer|
+				CustG.create!(
+			 		game_id: game.id,
+			  		customer_id: customer.id
+				)
+			end
+			FunctionUsage.all.each do |usage|
+				if !usage.fixedPars && !usage.differentEachQ
+			      	if !usage.differentEachCust
+			    		FunctionG.create!(
+				  			game_id: game.id,
+			      			function_usage_id: usage.id
+		    			)
+			      	else
+			    		Customer.all.each do |customer|
+			  	  			CustGFunc.create!(
+			  	    			customer_id: customer.id,
+				    			game_id: game.id,
+			        			function_usage_id: usage.id
+			  	  			)
+		    			end
+		  			end
+				end
+			end
+		else
+		  	# extract simulation results here
+			usage_id = FunctionUsage.where(version_id: game.version_id, name: "demand").take.id
+			demandPer = q.rangeDemandPer
+			demandFunc = FunctionQ.where(quarter_id: q.id, function_usage_id: usage_id).take
+			if demandFunc.function_id.nil? # choose a random function for demand if not selected (parameters will be auto assigned)
+				demandFunc.update_attributes(
+					function_id: Function.where(version_id: game.version_id, function_usage_id: usage_id).take.id
+				)
+				demandFunc = FunctionQ.find(demandFunc.id)
+			end
+			parentFunc = Function.find(demandFunc.function_id)
+			a = demandFunc.parA * parentFunc.parAscale unless demandFunc.parA.nil?
+			b = demandFunc.parB * parentFunc.parBscale unless demandFunc.parB.nil?
+			c = demandFunc.parC * parentFunc.parCscale unless demandFunc.parC.nil?
+			d = demandFunc.parD * parentFunc.parDscale unless demandFunc.parD.nil?
+			fist_month = Month.find(game.first_month_id).no
+			offset = 3 * (q.q_no - 1)
+			current_month = offset + fist_month - 1 # - 1, cause number starts from 1
+			for i in 0..2
+				m = (current_month + i) % 12 + 1 # + 1, cause number starts from 1
+				month = Month.where(no: m).take
+				x = offset + i
+				demandP = eval(Function.find(demandFunc.function_id).function)*month.seasonPer/100
+				demandMin = (demandP*(100-demandPer)/100).round(0)
+				demandMax = (demandP*(100+demandPer)/100).round(0)
+				demandS = rand(demandMin..demandMax)
+				mq = Mq.create!(
+					quarter_id: q.id,
+					month_id: month.id,
+					demandPmin: demandMin,
+					demandPmax: demandMax,
+					demandS: demandS
+				)
+				marketsizeArr = []
+				custCityMap = []
+				demandPArr = []
+				demandSArr = []
+				marketsizeMin = 0
+				marketsizeMax = 0 # keep max to define loop times
+				scale = 5 # taken from CreateCustCities
+				integer = 10**5
+				# n: demand
+				# m: cust_city entries
+				# O(n) linear generate/save of demand and O(m) linear searches/insertions in database => fastest way since m << n
+				CustCity.order(marketsizeTotRmin: :asc).all.each do |custcity|
+					demandPArr[custcity.id] = ((custcity.marketsizeTotPer*demandP)/100).round(0) # predicted demand (exists for visual comparison with simulation)
+					marketsizeMin = (custcity.marketsizeTotRmin*integer).round(0).to_i
+					marketsizeMax = (custcity.marketsizeTotRmax*integer).round(0).to_i
+					for k in marketsizeMin..marketsizeMax
+						marketsizeArr[k] = custcity.id
+					end
+				end
+				for j in 1..demandS
+					marketsize = rand(0..marketsizeMax)
+					demandSArr[marketsizeArr[marketsize]] = demandSArr[marketsizeArr[marketsize]].to_i + 1
+				end
+				demandSArr.each_with_index do |demandS, cust_city_id|
+					CustGMqCity.create!(
+						mq_id: mq.id,
+						cust_city_id: cust_city_id,
+						demandS: demandS || 0,
+						demandP: demandPArr[cust_city_id]
+					) unless cust_city_id==0
+				end
+			end
+	  		q_no = q.q_no + 1 # proceed to next quarter
+  		end
+	  	if q_no <= game.duration
+	  		quarter = Quarter.create!(
+			    game_id: game.id,
+			    q_no: q_no
+		  	)
+			game.update_attributes( # it is ovewritten in game model
+				current_quarter: q_no
+			)
+		  	Company.where(game_id: game.id).each do |comp|
+		  		CompQ.create!(
+	    	  		company_id: comp.id,
+		      		quarter_id: quarter.id
+		  		)
+		  	end
+		  	FunctionUsage.all.each do |usage|
+		  		if !usage.fixedPars && usage.differentEachQ
+	  	      		if !usage.differentEachCust
+			    		FunctionQ.create!(
+		    	  			quarter_id: quarter.id,
+			      			function_usage_id: usage.id
+			    		)
+	  	      		else
+		   				Customer.all.each do |customer|
+			      			CustQFunc.create!(
+			      				quarter_id: quarter.id,
+	      	        			customer_id: customer.id,
+		            			function_usage_id: usage.id
+	  	  	      			)
+		    			end
+		  			end
+		  		end
+		  	end
+	  	else
+		  game.update_attributes(
+		    current_quarter: (game.duration+1),
+		    finished: true
+	  	  )
+		end
+		if q_no > 1
+  			redirect_to(admin_cust_g_mq_cities_path, flash: {notice: 'You can see the results of Demand in this Page.', warning: 'Change default Parameters in Tables with Q before proceeding'})
+
+  		else
+  			redirect_to(admin_quarters_path, flash: {warning: 'Fill Tables with Q and G before starting the Game'})
+		end
+  	end
+
+	show do
+		default_main_content
+		q = Quarter.where(game_id: params[:id]).order(q_no: :desc).take
+		strong (button_to "Add CEOs", '/admin/companies#new') if q.nil?
+	end
 end
