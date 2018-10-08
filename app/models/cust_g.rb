9cdec0b22fe9bbe8d3fc6def7d9da7c22e1fa46a
Nikolaos I. Tzanetis <tzanetis@outlook.com>
2018-10-09 01:17:36 +0300

    Add methods before changing the Models to Perform Automatic Tasks 2.

--- app/models/cust_g.rb
+++ app/models/cust_g.rb
@@ -1,4 +1,89 @@
 class CustG < ApplicationRecord
-  belongs_to :game
-  belongs_to :customer
+  	belongs_to :customer
+  	belongs_to :game
+
+  	validates :game, uniqueness: {scope: :customer}, presence: true
+	validates :rebuyPer, numericality: true, allow_nil: true
+	validates :rebuyInterval, numericality: true, allow_nil: true
+	validates :A1_prod_char_SI, numericality: true, allow_nil: true
+	validates :A2_adv_char_SI, numericality: true, allow_nil: true
+	validates :A3_price_SI, numericality: true, allow_nil: true
+	validates :A4_quality_SI, numericality: true, allow_nil: true
+
+  	before_save :customer_save
+
+	private
+		def customer_save
+		  	cust = Customer.find(self.customer_id)
+		  	scale = (10 ** cust.scales)
+		  	decimals = cust.scales - 2
+		    step = 1.0/scale * 100
+		    cust.rebuyRmin = cust.rebuyRmin * scale unless cust.rebuyRmin.nil?
+		    cust.rebuyRmax = cust.rebuyRmax * scale unless cust.rebuyRmax.nil?
+		    cust.rebuyIntervalmin = cust.rebuyIntervalmin unless cust.rebuyIntervalmin.nil?
+		    cust.rebuyIntervalmax = cust.rebuyIntervalmax unless cust.rebuyIntervalmax.nil?
+		    if cust.rebuyIntervalmax.nil?
+		    	cust.rebuyIntervalmax = Game.find(self.game_id).duration + 1
+		    end
+		    cust.A1_prod_char_SI_min = cust.A1_prod_char_SI_min * scale
+		    cust.A1_prod_char_SI_max = cust.A1_prod_char_SI_max * scale
+		    cust.A2_adv_char_SI_min = cust.A2_adv_char_SI_min * scale
+		    cust.A2_adv_char_SI_max = cust.A2_adv_char_SI_max * scale
+		    cust.A3_price_SI_min = cust.A3_price_SI_min * scale
+		    cust.A3_price_SI_max = cust.A3_price_SI_max * scale
+		    cust.A4_quality_SI_min = cust.A4_quality_SI_min * scale
+		    cust.A4_quality_SI_max = cust.A4_quality_SI_max * scale
+
+	        if self.rebuyPer.nil?
+          		self.rebuyPer = step * rand(cust.rebuyRmin..cust.rebuyRmax)
+	        else
+				self.rebuyPer = self.rebuyPer.round(decimals)
+				if self.rebuyPer > cust.rebuyRmax
+					self.rebuyPer = cust.rebuyRmax
+				elsif self.rebuyPer < cust.rebuyRmin
+					self.rebuyPer = cust.rebuyRmin
+				end
+	        end
+	        if self.rebuyInterval.nil?
+          		self.rebuyInterval = rand(cust.rebuyIntervalmin..cust.rebuyIntervalmax)
+	        else
+				self.rebuyInterval = self.rebuyInterval.round(0)
+				if self.rebuyInterval > cust.rebuyIntervalmax
+					self.rebuyInterval = cust.rebuyIntervalmax
+				elsif self.rebuyInterval < cust.rebuyIntervalmin
+					self.rebuyInterval = cust.rebuyIntervalmin
+				end
+	        end
+	        if self.A1_prod_char_SI.nil?
+          		self.A1_prod_char_SI = step * rand(cust.A1_prod_char_SI_min..cust.A1_prod_char_SI_max)
+	        else
+				self.A1_prod_char_SI = self.A1_prod_char_SI.round(decimals)
+				if self.A1_prod_char_SI > cust.A1_prod_char_SI_max
+					self.A1_prod_char_SI = cust.A1_prod_char_SI_max
+				elsif self.A1_prod_char_SI < cust.A1_prod_char_SI_min
+					self.A1_prod_char_SI = cust.A1_prod_char_SI_min
+				end
+	        end
+	        if self.A2_adv_char_SI.nil?
+          		self.A2_adv_char_SI = step * rand(cust.A2_adv_char_SI_min..cust.A2_adv_char_SI_max)
+	        else
+				self.A2_adv_char_SI = self.A2_adv_char_SI.round(decimals)
+				if self.A2_adv_char_SI > cust.A2_adv_char_SI_max
+					self.A2_adv_char_SI = cust.A2_adv_char_SI_max
+				elsif self.A2_adv_char_SI < cust.A2_adv_char_SI_min
+					self.A2_adv_char_SI = cust.A2_adv_char_SI_min
+				end
+	        end
+	        if self.A3_price_SI.nil?
+          		self.A3_price_SI = step * rand(cust.A3_price_SI_min..cust.A3_price_SI_max)
+	        else
+				self.A3_price_SI = self.A3_price_SI.round(decimals)
+				if self.A3_price_SI > cust.A3_price_SI_max
+					self.A3_price_SI = cust.A3_price_SI_max
+				elsif self.A3_price_SI < cust.A3_price_SI_min
+					self.A3_price_SI = cust.A3_price_SI_min
+				end
+	        end
+		  	self.A4_quality_SI = (100-self.A1_prod_char_SI-self.A2_adv_char_SI-self.A3_price_SI).round(cust.scales-2)
+		end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,12 +2,19 @@ class Game < ApplicationRecord
   belongs_to :version
   belongs_to :first_month, class_name: 'Month'
 
-  has_many :cust_g_funcs
   has_many :quarters
-  has_many :cust_g_funcs
+  has_many :function_gs
   has_many :cust_gs
-  has_many :chat_gs
+  has_many :cust_g_funcs
   has_many :companies
+  has_many :chat_gs
+
+  validates :name, uniqueness: {scope: :version}, presence: true
+  validates :starting_year, numericality: {greater_than: 1990, less_than_or_equal_to: Time.current.year, only_integer: true}
+  validates :duration, numericality: true, allow_nil: true
+  validates :trainExtraAvail, numericality: true, allow_nil: true
+  validates :illPer, numericality: true, allow_nil: true
+  validates :current_quarter, numericality: true, allow_nil: true
 
   before_create :init_quarter
 
@@ -24,19 +31,17 @@ class Game < ApplicationRecord
   private
     def update_quarter
       q = Quarter.where(game_id: self.id).order(q_no: :desc).take
-      if q
-        if q.q_no < self.duration
-          self.current_quarter =  q.q_no
-        end
-      else
+      if q.nil?
         self.current_quarter =  0
+      elsif q.q_no < self.duration
+        self.current_quarter =  q.q_no
       end
     end
 
-  before_save :duration_game
+  before_save :game_save
 
   private
-    def duration_game
+    def game_save
       version = Version.find(self.version_id)
       if self.duration.nil?
         self.duration = version.durationDef
@@ -44,6 +49,22 @@ class Game < ApplicationRecord
         self.duration = version.durationMax
       elsif self.duration < version.durationMin
         self.duration = version.durationMin
+      end
+      if self.trainExtraAvail.nil?
+        self.trainExtraAvail = self.duration + 1
+      elsif self.trainExtraAvail > version.durationMax
+        self.trainExtraAvail = version.durationMax
+      elsif self.trainExtraAvail < 1
+        self.trainExtraAvail = 1
+      end
+      version.illPerRmin = (version.illPerRmin*100).round(0)
+      version.illPerRmax = (version.illPerRmax*100).round(0)
+      if self.illPer.nil?
+        self.illPer = rand(version.illPerRmin..version.illPerRmax)
+      elsif self.illPer > version.illPerRmax
+        self.illPer = version.illPerRmax
+      elsif self.illPer < version.illPerRmin
+        self.illPer = version.illPerRmin
       end
     end
 end
